519190ac0868a1b1f32a028b460e4a7ef44fb872
jenny <88226911+kmjenny@users.noreply.github.com>
2023-08-28 19:35:01 +0900

    feature/21: Modal API

--- notice/views.py
+++ notice/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets, mixins
 
 from .models import Notification, Modal
-from .serializers import NotificationSerializer, NotificationDetailSerializer
+from .serializers import (
+    NotificationSerializer,
+    NotificationDetailSerializer,
+    ModalSerializer,
+)
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
@@ -21,4 +25,19 @@ class NotificationViewSet(
         queryset = Notification.objects.filter(
             Q(published_at__date__lte=today)
         ).order_by("-published_at")
+        return queryset
+
+
+class ModalViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = ModalSerializer
+
+    def get_queryset(self):
+        today = datetime.now().date()
+
+        queryset = Modal.objects.filter(
+            Q(published_at__date__lte=today)
+            & Q(ended_at__date__gte=today)
+            & Q(is_active=True)
+        ).order_by("-id")[:1]
+
         return queryset
