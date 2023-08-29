2837d024d73fcf0a1aea83833b50795d33aa8ec9
0seoYun <yys020819@gmail.com>
2023-08-29 23:43:14 +0900

    fix/29 : viewset/admin search fields 수정

--- post/views.py
+++ post/views.py
@@ -23,7 +23,7 @@ class PostViewSet(
 ):
     pagination_class = PostPagination
     filter_backends = [TypeFilter, SearchFilter]
-    search_fields = ["content"]
+    search_fields = ["title", "content"]
 
     def get_serializer_class(self):
         if self.action == "retrieve":
